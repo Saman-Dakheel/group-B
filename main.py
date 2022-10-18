1932881434308bcd763375b31a6db53f6cf2087e
saman dakheel <saman.dakheel.a@gmail.com>
2022-10-18 21:23:35 +0300

    this is commit

---
+++ main.py
@@ -0,0 +1,143 @@
+# i = 9
+# print(id(i))
+# for i in range(11):
+#     print(i)
+#     print(id(i))
+# print(id(i))
+
+
+# try:
+#     x = float(input('enter number : '))
+#     print(float(5 / x))
+# except ZeroDivisionError as e:
+#     print("{}".format(e))
+# print("Finished")
+
+# try:
+#     x = float(input('enter number : '))
+#     print(float(5 / x))
+# except :
+#     print("Exception Occurred")
+# print("Finished")
+
+
+# try:
+#     x = float(input('enter number : '))
+#     y =float(5 / x)
+#     print(y)
+# except ZeroDivisionError as e:
+#     print("{}".format(e))
+# finally:
+#     y = y + 2
+#     print(y)
+
+# try:
+#     f = open('test_file.txt')
+#     print("file found")
+# except FileNotFoundError as e:
+#     print("{}".format(e))
+# finally:
+#     print("Finally")
+
+# try:
+#     f = open('test_file1.txt')
+#     print("file found")
+# except FileNotFoundError as e:
+#     print("{}".format(e))
+# else:
+#     print("else")
+# finally:
+#     print("Finally")
+
+
+# try:
+#     f = open('test_file1.txt')
+#     print("file found")
+# except ValueError as e:
+#     print("{}".format(e))
+# finally:
+#     print("Finally")
+
+
+#########Group B###################################################
+
+
+# if i < 7:
+#     print(i)
+
+
+#
+# x = int(input('number : '))
+# print(int(4/x))
+
+
+# try:
+#     x = int(input('number : '))
+#     print(int(4 / x))
+# except :
+#     print('You can not divide by zero')
+
+
+# try:
+#     x = int(input('number : '))
+#     print(int(4 / x))
+# except :
+#     print('You can not divide by zero')
+# finally:
+#     print('Finally')
+
+
+# try:
+#     x = int(input('number : '))
+#     print(int(4 / x))
+# except ZeroDivisionError as ghada:
+#     print(ghada)
+# finally:
+#     print("finally done")
+
+
+# try:
+#     f = open('test_file.txt')
+#     print('file created')
+# except FileNotFoundError as e:
+#     print(e)
+# else:
+#     print("No exception executed")
+# finally:
+#     print("Executing Finally ...")
+
+
+#
+# try:
+#     file = open('test_file.txt')
+#     print('file open here')
+# except:
+#     print("file not found ")
+# else:
+#     print('file is ready')
+# finally:
+#     print('done')
+
+#
+# try:
+#     amount = 3999
+#     if amount < 2999:
+#         raise ValueError("please add money in your account")
+#     else:
+#         print("You are eligible to purchase DSA Self Paced course")
+# except ValueError as e:
+#     print(e)
+# else:
+#     print('else executed ')
+
+
+#eating (counting) 21 KOTELK
+# eat = ["KOTELK"]
+# for i in range (1,22):
+#     for j in eat:
+#         print(i, j)
+
+
+
+# for i in range(6):
+#     print('*' * 11)
